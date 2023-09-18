08d14b7668401e68728383addc4e387cb1f370f6
still <kenda.dennis3@gmail.com>
2023-09-18 12:32:28 +0300

    Add pomp to the Navi

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import prof from '../assets/images/prof.jpg'
+import profls from '../assets/images/profLS.png'
 import '../index.css'
 
 function Hero() {
@@ -6,10 +8,13 @@ function Hero() {
         <>
             <div className='hero'>
                 <div className='heroleft'>
-                    <h1>Hi, My name is Vlad</h1>
-                    <p></p>
+                    <h1>Hi, My name is <span className='intro'>Vlad</span></h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Rem sequi quasi quam eius consequatur aliquam dicta!
+                        Quidem odio blanditiis ab! Labore quam cupiditate eius
+                        ipsum recusandae vero iste consequatur velit.</p>
                 </div>
-                <div className='heroright'><img src={"./assets/images/prof.jpg"} /></div>
+                <div className='heroright'><img src={prof} /></div>
 
 
             </div>
--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -7,14 +7,13 @@ function Navi() {
     const [theme, toggletheme] = useState(true);
     return (
         <>
-            <h2>Hello world</h2>
             <div className='navi'>
                 <a href='home'>Home</a>
                 <ul>
-                    <li><a href='home'>1</a></li>
-                    <li><a href='home'>2</a></li>
-                    <li><a href='home'>3</a></li>
-                    <li><a href='home'>4</a></li>
+                    <li><a href='#home'>Home</a></li>
+                    <li><a href='#home'>Projects</a></li>
+                    <li><a href='#home'>Contact</a></li>
+                    <li><a href='#home'>About</a></li>
 
 
                 </ul>
@@ -22,12 +21,13 @@ function Navi() {
                 <button id='togglemenu'><i className={sidenavi ? 'fa fa-bars ' : 'fa fa-bars'}></i></button>
 
             </div >
+            <br />
             <div className='sidenavi'>
                 <ul>
-                    <li><a href='home'>1</a></li>
-                    <li><a href='home'>2</a></li>
-                    <li><a href='home'>3</a></li>
-                    <li><a href='home'>4</a></li>
+                    <li><a href='#home'>Home</a></li>
+                    <li><a href='#home'>Projects</a></li>
+                    <li><a href='#home'>Contact</a></li>
+                    <li><a href='#home'>About</a></li>
                 </ul>
             </div>
         </>
