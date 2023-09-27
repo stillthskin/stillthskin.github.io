6f5536be59d2746840e1e4a18ae7213bdd67d7cf
stillthskin <kenda.dennis3@gmail.com>
2023-09-27 21:42:18 +0300

    There is another try i think at this point i am just guessing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  "base": "/solardeploy/"
+  base: "https://stillthskin.github.io/portfolio/"
 })
