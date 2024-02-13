d2db5ffed15f72e2e32a0ba16cd02db863bc0b5e
Still <kenda.dennis3@gmail.com>
2024-02-13 12:45:43 +0300

    Timeline edit 13_02_24

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,52 +1,78 @@
 import React from "react"
 
 
-function Timeline(){
-    return(
+function Timeline() {
+    return (
         <>
-         
-        <div className="timeline">
-        <h4>Career:</h4>
-        <div className="container">
-            <div className="component">
-                <div className="timeline_date date_right">2024 January 8TH</div>
+
+            <div className="timeline">
+                <h4>Career:</h4>
+                <div className="container">
+                    <div className="component">
+                        <div className="timeline_date date_right">2024 January 8TH</div>
+                    </div>
+
+                    <div className="component_middle">
+                        <div className="timeline_dot"></div>
+                    </div>
+                    <div className="component">
+                        <div className="timeline_date achivement"><h4><b><u>Smart-People Africa Ltd</u></b></h4>
+                            <ul>
+                                <li>• Design and implement server-side logic using Java and Tomcat.</li>
+                                <li>• Develop and maintain database schema and queries using PostgreSQL.</li>
+                                <li>• Optimize and tune the performance of back-end services.</li>
+                                <li>• Configure and manage deployment pipelines using Git for version control.</li>
+                                <li>• Automate build, test, and deployment processes.</li>
+                                <li>• Monitor system performance and troubleshoot issues.</li>
+                                <li>• Manage and optimize PostgreSQL databases.</li>
+                                <li>• Ensure data integrity and security.</li>
+                                <li>• Backup and restore databases as needed.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="component">
+                        <div className="timeline_date achivement"><h4><b>Kenya Institute of Public Policy Research & Analysis(KIPPRA)</b></h4>
+                            <ul>
+                                <li>• Reviewed diagnostics and assessed the functionality and efficiency of systems.</li>
+                                <li>• Implemented security measures and monitored security certificates.</li>
+                                <li>• Offered technical support to company staff, troubleshooting computer problems.</li>
+                                <li>• Installed and updated company software and hardware as needed</li>
+                                <ul>• Anticipated and reported the cost of replacing or updating computer items.</ul>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div className="component_middle">
+                        <div className="timeline_dot"></div>
+                    </div>
+                    <div className="component">
+                        <div className="timeline_date date_left">Jan 2023</div>
+                    </div>
+                    <div className="component">
+                        <div className="timeline_date date_right">Jun 2019</div>
+                    </div>
+
+                    <div className="component_middle">
+                        <div className="timeline_dot"></div>
+                        <div className="timeline_dot timeline_dot--bottom"></div>
+                    </div>
+                    <div className="component">
+                        <div className="timeline_date achivement"><h4><b>National Treasury(PFMR)</b></h4>
+                            <ul>
+                                <li>Support the IT team in maintaining hardware, software, and other systems.</li>
+                                <li>Assist with troubleshooting issues and provide technical support.</li>
+                                <li>Organize and maintain IT resources.</li>
+                                <li>Lend IT support in areas such as cybersecurity, programming, analytics, and data
+                                    center management.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
-            <div className="component_middle">
-                <div className="timeline_dot"></div>
-            </div>
-            <div className="component">
-                <div className="timeline_date achivement"><h4><b><u>Smart-People Africa Ltd</u></b></h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis impedit</p></div>
-            </div>
-            <div className="component">
-                <div className="timeline_date achivement"><h4><b>Kenya Institute of Public Policy Research & Analysis(KIPPRA)</b></h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis impedit</p></div>
-            </div>
-            <div className="component_middle">
-                <div className="timeline_dot"></div>
-            </div>
-            <div className="component">
-                <div className="timeline_date date_left">2024 January 8TH</div>
-            </div>
-            <div className="component">
-                <div className="timeline_date date_right">2024 January 8TH</div>
-            </div>
-            
-            <div className="component_middle">
-                <div className="timeline_dot"></div>
-                <div className="timeline_dot timeline_dot--bottom"></div>
-            </div>
-            <div className="component">
-                <div className="timeline_date achivement"><h4><b>National Treasury(PFMR)</b></h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis impedit</p></div>
-            </div>
-        </div>
-        </div>
         </>
     )
-        
 
-    
+
+
 }
 export default Timeline
